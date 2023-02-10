ef3c923971aa8585ce80e685d52e3999d2cb87f5
mike <mike160384@hotmail.com>
2023-02-10 15:23:21 +0800

    committed by mike
    bbbb

--- models/years_ex.py
+++
@@ -1,9 +0,0 @@
-from main import db
-
-
-class Years(db.Model):
-    __tablename__ = "years_ex"
-    
-    ex_id = db.Column(db.Integer, primary_key=True, nullable=False)
-    spec_id = db.Column(db.Integer, db.ForeignKey("specialties.spec_id"), nullable=False)
-    prac_id = db.Column(db.Integer, db.ForeignKey("practitioners.prac_id"), nullable=False)
